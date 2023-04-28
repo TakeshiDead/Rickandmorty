a838eda49fa02254446491c28a7f52acdc589b22
Alex <oniel212@hotmail.com>
2023-04-28 05:30:46 -0400

    Changes on the error catch and clean up the code.

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -5,33 +5,28 @@ import './App.js';
 
 
 
-const SearchResults = ({ results, error}) => {
-  if(results && results.length) {
-  return (
-    <div className="search-results">
-      
-      
-      {results.map(result => (
-        <div key={result.id} className="search-result">
-          <img src={result.image} alt={result.name} />
-          <h3>{result.name }</h3>
-          <p>{result.species}</p>
-          <p>{result.gender}</p>
-          <p>{result.status}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
-if(error){
+const SearchResults = ({ results, error }) => {
+  if (results && results.length) {
+    return (
+      <div className="search-results">
+
+
+        {results.map(result => (
+          <div key={result.id} className="search-result">
+            <img src={result.image} alt={result.name} />
+            <h3>{result.name}</h3>
+            <p>{result.species}</p>
+            <p>{result.gender}</p>
+            <p>{result.status}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return <div>
-    <div className='container_404 container_gif'><img src="https://media.tenor.com/AtKPpo2MXFEAAAAd/dog-burning.gif" alt="Rick And Morty, HD Png Download@kindpng.com" style={{display: 'flex', justifyContent: 'center', alignItems: 'center' }}></img></div>
-  <div> <h1 className='container_404'>404</h1></div>
-  <div className='container_404'><h2>characters not founds.</h2></div></div>
-}
-return<div>
-  <p>Write in the searchbar to start searching</p>
-</div>
+    <p>Write in the searchbar to start searching</p>
+  </div>
 };
 
 export default SearchResults;
--- src/SkeletonResults.js
+++ src/SkeletonResults.js
@@ -3,12 +3,12 @@ import 'react-loading-skeleton/dist/skeleton.css'
 import './SearchResults.css';
 
 
-const SkeletonResults = ({cards}) => {
+const SkeletonResults = ({ cards }) => {
   return (
-   
-    Array(cards).fill(0).map((_, i) => 
-    
-      <div className='search-results'key={i} >
+
+    Array(cards).fill(0).map((_, i) =>
+
+      <div className='search-results' key={i} >
         <div className="search-result">
           <Skeleton square width={206.67} height={206.67} />
           <h3><Skeleton /></h3>
@@ -17,7 +17,7 @@ const SkeletonResults = ({cards}) => {
 
         </div>
       </div>
-   ))
+    ))
 }
 
 export default SkeletonResults
